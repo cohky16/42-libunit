135e9a988d880f4c6d9567e98f22c13f45ab4c17
yasuda koki <k.yasuda.0106@gmail.com>
2021-05-15 11:07:39 +0900

    remove comment

--- framework/libunit.c
+++ framework/libunit.c
@@ -6,13 +6,12 @@
 /*   By: kyasuda <kyasuda@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/08 15:08:20 by kyasuda           #+#    #+#             */
-/*   Updated: 2021/05/14 20:48:11 by yyamagum         ###   ########.fr       */
+/*   Updated: 2021/05/15 11:06:55 by kyasuda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libunit.h"
 
-// 構造体の作成
 static	t_unit_test	*new_unit_test(char *name, int(*function)(void))
 {
 	t_unit_test	*new;
@@ -25,8 +24,6 @@ static	t_unit_test	*new_unit_test(char *name, int(*function)(void))
 	return (new);
 }
 
-// テスト関数を読み込む
-// e.g. load_test(&testlist, "Basic test", &basic_test);
 void	load_test(t_unit_test **testlist, char *name, int (*function)(void))
 {
 	t_unit_test	*new;
@@ -36,7 +33,6 @@ void	load_test(t_unit_test **testlist, char *name, int (*function)(void))
 	*testlist = new;
 }
 
-// プロセスidのチェック
 static	int	check_pid(pid_t pid)
 {
 	if (WIFEXITED(pid))
@@ -63,7 +59,6 @@ static	int	check_pid(pid_t pid)
 	return (0);
 }
 
-// 子プロセスの作成とテストの実行
 static	int	exec_test(int (*function)(void))
 {
 	pid_t				pid;
@@ -86,8 +81,6 @@ static	int	exec_test(int (*function)(void))
 	return (check_pid(pid));
 }
 
-// テスト関数を実行する
-// e.g. launch_tests(&testlist));
 int	launch_tests(t_unit_test **testlist)
 {
 	t_unit_test	*test;
--- real-tests/libft/ft_strlcpy.c
+++ real-tests/libft/ft_strlcpy.c
@@ -6,13 +6,12 @@
 /*   By: kyasuda <kyasuda@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/23 22:17:42 by kyasuda           #+#    #+#             */
-/*   Updated: 2021/05/15 06:26:39 by yyamagum         ###   ########.fr       */
+/*   Updated: 2021/05/15 11:07:21 by kyasuda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-// ok
 size_t	ft_strlcpy(char *dst, const char *src,
 					size_t dstsize)
 {
@@ -33,25 +32,3 @@ size_t	ft_strlcpy(char *dst, const char *src,
 	dst[i] = '\0';
 	return (len);
 }
-
-// // ko
-// size_t	ft_strlcpy(char *dst, const char *src,
-// 					size_t dstsize)
-// {
-// 	size_t	len;
-// 	size_t	i;
-
-// 	if (!src)
-// 		return (0);
-// 	len = ft_strlen(src);
-// 	if (!dst || !dstsize)
-// 		return (len);
-// 	i = 0;
-// 	while (src[i] && i < dstsize - 1)
-// 	{
-// 		dst[i] = src[i];
-// 		i++;
-// 	}
-// 	dst[i] = '\0';
-// 	return (42);
-// }
